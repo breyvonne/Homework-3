0f79ae538e3c15fa32d5fccd0b5db06684aeb698
Breanna <123026437+breyvonne@users.noreply.github.com>
2023-04-04 09:52:56 -0400

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,69 @@
+# #breanna eafon
+#
+# # words=[word1,word2,word3,word4]
+# # my_string="placeholder"
+# #
+# #
+# def freq(my_string,substring):
+#     count=0
+#     for ch in my_string:
+#         if ch==substring:
+#             count+=1
+#         return count
+#
+#
+#
+# my_string="there was a  dog in the sun drinking water dog sad."
+# substring=words
+# def freq(my_string,substring):
+#     count=0
+#     for word in words:
+#         if substring in word:
+#             count+=1
+#     return count
+#
+#
+# print(freq(my_string,"dog"))
+# print(freq(my_string,"sun"))
+# print(freq(my_string,"cat"))
+#
+#
+# def locate_string(my_string,substring):
+#     if substring [:len(my_string)]== substring:
+#         return "This word", substring, " shows up in the Beginning"
+#     elif my_string[-len(substring):]==substring:
+#         return "this word" ,substring," shows up in the end"
+#     elif substring in my_string:
+#         return " this word",substring, "appears in the middle"
+#     else:
+#         return"not found"
+#
+#
+# print(locate_string(my_string,substring))
+
+#Breanna Eafon Homework #3
+#State of the Union Address Barack Obama
+speech ="Mr. Speaker, Mr. Vice President, Members of Congress, my fellow Americans:Tonight marks the eighth year that I’ve come here to report on the State of the Union. And for this final one, I’m going to try to make it a little shorter. (Applause.) I know some of you are antsy to get back to Iowa. (Laughter.) I've been there. I'll be shaking hands afterwards if you want some tips. (Laughter.) And I understand that because it’s an election season, expectations for what we will achieve this year are low. But, Mr. Speaker, I appreciate the constructive approach that you and the other leaderstook at the end of last year to pass a budget and make tax cuts permanent for working families. So I hope we can work together this year on some bipartisan priorities like criminal justice reform -- (applause) -- and helping people who are battling prescription drug abuse and heroin abuse. (Applause.) So, who knows, we might surprise the cynics again.But tonight, I want to go easy on the traditional list of proposals for the year ahead. Don’t worry, I’ve got plenty, from helping students learn to write computer code to personalizing medical treatments for patients. And I will keep pushing for progress on the work that I believe still needs to be done. Fixing a broken immigration system. (Applause.) Protecting our kids from gun violence. (Applause.)Equal pay for equal work. (Applause.) Paid leave. (Applause.)Raising the minimum wage. (Applause.) All these things still matter to hardworking families. They’re still the right thing to do. And I won't let up until they get done.But for my final address to this chamber, I don’t want to just talk about next year. I want to focus on the next five years, the next 10 years, and beyond. I want to focus on our future.We live in a time of extraordinary change -- change that’s reshaping the way we live, the way we work, our planet, our place in the world. It’s change that promises amazing medical breakthroughs, but also economic disruptions that strain working families. It promises education for girls in the most remote villages, but also connects terrorists plotting an ocean away. It’s change that can broaden opportunity, or widen inequality. And whether we like it or not, the pace of this change will only accelerate.America has been through big changes before -- wars and depression, the influx of new immigrants, workers fighting for a fair deal, movements to expand civil rights. Each time, there have been those who told us to fear the future; who claimed we could slam the brakes on change; who promised to restore past glory if we just got some group or idea that was threatening America under control. And each time, we overcame those fears. We did not, in the words of Lincoln, adhere to the “dogmas of the quiet past.” Instead we thought anew, and acted anew. We made change work for us, always extending America’s promise outward, to the next frontier, to more people. And because we did -- because we saw opportunity where others saw only peril -- we emerged stronger and better than before.What was true then can be true now. Our unique strengths as a nation -- our optimism and work ethic, our spirit of discovery, our diversity, our commitment to rule of law -- these things give us everything we need to ensure prosperity and security for generations to come.In fact, it’s that spirit that made the progress of these past seven years possible.  It’s how we recovered from the worst economic crisis in generations.  It’s how we reformed our health care system, and reinvented our energy sector; how we delivered more care and benefits to our troops and veterans, and how we secured the freedom in every state to marry the person we love.But such progress is not inevitable. It’s the result of choices we make together. And we face such choices right now. Will we respond to the changes of our time with fear, turning inward as a nation, turning against each other as a people? Or will we face the future with confidence in who we are, in what we stand for, in the incredible things that we can do together?So let’s talk about the future, and four big questions that I believe we as a country have to answer -- regardless of who the next President is, or who controls the next Congress.First, how do we give everyone a fair shot at opportunity and security in this new economy? (Applause.)Second, how do we make technology work for us, and not against us -- especially when it comes to solving urgent challenges like climate change? (Applause.)Third, how do we keep America safe and lead the world without becoming its policeman? (Applause.)And finally, how can we make our politics reflect what’s best in us, and not what’s worst?Let me start with the economy, and a basic fact: The United States of America, right now, has the strongest, most durable economy in the world. (Applause.) We’re in the middle of the longest streak of private sector job creation in history. (Applause.) More than 14 million new jobs, the strongest two years of job growth since the ‘90s, an unemployment rate cut in half. Our auto industry just had its best year ever. (Applause.) That's just part of a manufacturing surge that's created nearly 900,000 new jobs in the past six years.And we’ve done all this while cutting our deficits by almost three-quarters. (Applause.)Anyone claiming that America’s economy is in decline is peddling fiction. (Applause.) Now, what is true -- and the reason that a lot of Americans feel anxious -- is that the economy has been changing in profound ways, changes that started long before the Great Recession hit; changes that have not let up.Today, technology doesn’t just replace jobs on the assembly line, but any job where work can be automated. Companies in a global economy can locate anywhere, and they face tougher competition. As a result, workers have less leverage for a raise. Companies have less loyalty to their communities. And more and more wealth and income is concentrated at the very top.All these trends have squeezed workers, even when they have jobs; even when the economy is growing. It’s made it harder for a hardworking family to pull itself out of poverty, harder for young people to start their careers, tougher for workers to retire when they want to. And although none of these trends are unique to America, they do offend our uniquely American belief that everybody who works hard should get a fair shot.For the past seven years, our goal has been a growing economy that works also better for everybody. We’ve made progress. But we need to make more. And despite all the political arguments that we’ve had these past few years, there are actually some areas where Americans broadly agree.We agree that real opportunity requires every American to get the education and training they need to land a good-paying job. The bipartisan reform of No Child Left Behind was an important start, and together, we’ve increased early childhood education, lifted high school graduation rates to new highs, boosted graduates in fields like engineering. In the coming years, we should build on that progress, by providing Pre-K for all and -- (applause) -- offering every student the hands-on computer science and math classes that make them job-ready on day one. We should recruit and support more great teachers for our kids. (Applause.)And we have to make college affordable for every American. (Applause.) No hardworking student should be stuck in the red.We’ve already reduced student loan payments to 10 percent of a borrower’s income. And that's good. But now, we’ve actually got to cut the cost of college. (Applause.) Providing two years of community college at no cost for every responsible student is one of the best ways to do that, and I’m going to keep fighting to get that started this year. (Applause.) It's the right thing to do. (Applause.)"
+
+specific_words = ["American", "law", "progress", "future"]
+words = speech.split()
+specific_word_count = {word: 0 for word in specific_words}
+specific_word_location = []
+current_index = 0
+
+for word in words:
+    if word in specific_words:
+        specific_word_count[word] += 1
+        specific_word_location.append(current_index)
+    current_index += 1
+
+for word, count in specific_word_count.items():
+    print(f"The word '{word}' appears {count} times in the speech.")
+#the f at the beginning of a string is used to create a formatted string literal. It allows you to embed expressions inside string literals, by surrounding them with curly braces {}.
+for location in specific_word_location:
+    if location == 0:
+        print(f"The word '{words[location]}' appears at the beginning of the speech.")
+    elif location == len(words) - 1:
+        print(f"The word '{words[location]}' appears at the end of the speech.")
+    else:
+        print(f"The word '{words[location]}' appears in the middle of the speech.")
